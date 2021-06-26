c86254d82cf0a048508c024c13a4860f8b3fe97b
Lasitha Thilakarathna <37534587+lasithadilshan@users.noreply.github.com>
2021-06-26 17:48:59 +0530

    Initial commit

---
+++ app/src/main/java/lasitha/dilshan/toasty/MainActivity.java
@@ -0,0 +1,16 @@
+package lasitha.dilshan.toasty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toaster.simpleToast(this, "Subscribe to Lasitha Dilshan Thilakarathna : )");
+    }
+}
